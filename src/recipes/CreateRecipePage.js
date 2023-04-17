03e590e31e2b396c0a72ac5295623d505a874cf1
Aziz Kosber <a.kosber@gmail.com>
2023-04-17 19:23:04 -0400

    Switch CreateRecipePage Class->Function

--- src/recipes/CreateRecipePage.js
+++ src/recipes/CreateRecipePage.js
@@ -1,53 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import IngredientTable from '../ingredients/IngredientTable';
 import IngredientForm from '../ingredients/IngredientForm';
 import RecipeNameForm from './RecipeNameForm';
 
-class CreateRecipePage extends Component {
-    state = {
-        ingredients: [],
-        recipeName: ""
-    };
+const CreateRecipePage = () => {
 
-    removeIngredient = index => {
-        const { ingredients } = this.state;
-    
-        this.setState({
-            ingredients: ingredients.filter((ingredient, i) => { 
-                return i !== index;
+    const ingredientsInitialState = [{
+        name:"",
+        quantity:""
+    }]
+
+    const [ingredients, setIngredients] = useState(ingredientsInitialState);
+    const [recipeName, setRecipeName] = useState("");
+
+    const removeIngredient = ingredient => {
+        setIngredients(
+            ingredients.filter((i) => { 
+                return i !== ingredient;
             })
-        });
-    }
-
-    submitRecipeName = (recipeName) => {
-        this.setState({
-            recipeName: recipeName
-        })
-    } 
-
-    submitIngredient = ingredient => {
-        this.setState({
-            ingredients: [...this.state.ingredients, ingredient]
-        });
-    }
-
-    render() {
-        const { ingredients, recipeName } = this.state;
-        
-        return (
-            <div className="container">
-                <h1>Create a Recipe</h1>
-                <h2>{recipeName}</h2>
-                <IngredientTable
-                    ingredientData={ingredients}
-                    removeIngredient={this.removeIngredient}
-                />
-                <h3>Add New Ingredient</h3>
-                <IngredientForm handleSubmit={this.submitIngredient} />
-                <RecipeNameForm submitRecipeName={this.submitRecipeName}/>
-            </div>
         );
     }
+
+    const submitRecipeName = (recipeName) => {
+        setRecipeName(
+            recipeName
+        )
+    } 
+
+    const submitIngredient = ingredient => {
+        if (ingredient.ingredientName == '' || ingredient.quantity =='') return;
+
+        setIngredients(
+            [...ingredients, ingredient]
+        );
+    }
+    
+    return (
+        <div className="container">
+            <h1>Create a Recipe</h1>
+            <h2>{recipeName}</h2>
+            <IngredientTable
+                ingredientData={ingredients}
+                removeIngredient={removeIngredient}
+            />
+            <h3>Add New Ingredient</h3>
+            <IngredientForm submitIngredient={submitIngredient} />
+            {recipeName === '' ? <RecipeNameForm submitRecipeName={submitRecipeName} /> : null }
+            
+        </div>
+    );
+
 }
 
 export default CreateRecipePage;
