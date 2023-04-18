6e6fd1ea1dcc575e5ce3f83e2e19861272e4858f
Aziz Kosber <a.kosber@gmail.com>
2023-04-18 10:18:30 -0400

    Switch IngredientForm to function component

--- src/ingredients/IngredientForm.js
+++ src/ingredients/IngredientForm.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class IngredientForm extends Component {
+const IngredientForm = () => {
 
     initialState = {
         ingredientName: '',
